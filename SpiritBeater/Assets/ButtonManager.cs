369e93f9225f5a0a580f61968d51371d7d2b3dc1
harryxa <Callan2.Patay@live.uwe.ac.uk>
2017-10-25 13:13:17 +0100

    Menu

---
+++ SpiritBeater/Assets/ButtonManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour {
+
+
+	public Animator animator;
+
+	public void StartGame(string scene)
+	{
+		SceneManager.LoadScene (scene);
+
+	}
+
+
+	public void QuitGame()
+	{
+		Application.Quit ();
+	}
+
+	public void Controls()
+	{
+
+		animator.SetBool ("ControlsUp", !animator.GetBool ("ControlsUp"));
+
+
+	}
+
+
+}
