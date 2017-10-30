e33e6889dab59eda084a9b60b4530b93dbeb9ba2
h2-alcott <Harry2.Alcott@live.uwe.ac.uk>
2017-10-30 11:31:17 +0000

    SPIRIT HEALTH YO

---
+++ SpiritBeater/Assets/Scripts/SpiritHealth.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiritHealth : MonoBehaviour {
+
+	public float maxHealth = 100f;
+	public float currentHealth;
+	public GameObject healthBar;
+
+
+
+	// Use this for initialization
+	void Start ()
+	{
+		currentHealth = maxHealth;
+
+		//debug, runs decrease health every second
+		//InvokeRepeating ("DecreaseHealth", 1f, 1f);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void DecreaseHealth()
+	{
+		currentHealth -= 2f;
+
+		//scales numbers for healthbar
+		float calculateHealth = currentHealth / maxHealth;
+		SetHealthBar (calculateHealth);
+	}
+
+	public void SetHealthBar(float health)
+	{
+		healthBar.transform.localScale = new Vector3 (health, healthBar.transform.localScale.y, healthBar.transform.localScale.z);
+	}
+}
