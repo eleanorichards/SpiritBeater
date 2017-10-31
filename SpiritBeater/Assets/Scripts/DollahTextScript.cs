ee6dcea5745aaa18368d14d84979202ff41c0fb1
Tipe125 <timpenfold@hotmail.co.uk>
2017-10-31 21:20:06 +0000

    added UI for score

---
+++ SpiritBeater/Assets/Scripts/DollahTextScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DollahTextScript : MonoBehaviour {
+
+    public float score;
+    public GameObject player;
+    public GameObject txt;
+    
+	// Use this for initialization
+	void Start () {
+
+
+        txt = this.gameObject; 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        score = player.GetComponent<Dollah>().DollahScore;
+        GetComponent<UnityEngine.UI.Text>().text = ":" + score.ToString();
+		
+	}
+}
